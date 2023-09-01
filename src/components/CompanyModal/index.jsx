840fcf4c025195c956ab6c2a6caa6d0959be47c7
MatheusGesser <matheusmvg@hotmail.com>
2023-09-01 10:46:07 -0300

    feat: add Company and User modal's component

---
+++ src/components/CompanyModal/index.jsx
@@ -0,0 +1,157 @@
+/* eslint-disable react/prop-types */
+import {
+  Backdrop,
+  ModalContainer,
+  Title,
+  Button,
+  Item,
+} from "../../styles/components/Modal";
+import { RemoveIcon } from "../Icons/RemoveIcon";
+import { useForm } from "react-hook-form";
+import InputMask from "react-input-mask";
+import { useEffect, useState } from "react";
+import UserAPI from "../../services/users";
+import CompanyAPI from "../../services/companies";
+
+export function CompanyModal({
+  updateCompanies,
+  updateUsers,
+  closeModal,
+  values,
+  companies,
+  users,
+}) {
+  const { register, handleSubmit, reset } = useForm();
+
+  // Filtra usuários vinculados com  a empresa
+  const assignedUsers = values.id
+    ? users.filter((user) =>
+        companies
+          .find((company) => company.id == values.id)
+          .users.includes(user.id)
+      )
+    : []; // Caso esteja criando uma empresa, ela não possui usuários ainda
+
+  // Filtra usuários que *não estão* vinculados com  a empresa
+  const unassignedUsers = values.id
+    ? users.filter(
+        (user) =>
+          !companies
+            .find((company) => company.id == values.id)
+            .users.includes(user.id)
+      )
+    : users; // Caso esteja criando uma empresa, mostra todos usuários
+
+  // Define se é uma inserção ou edição de dado
+  async function handleSubmitForm(data) {
+    // Valida CNPJ completo
+    let cnpjInput = document.querySelector("input#cnpj");
+    if (data.cnpj.replace(/\D/g, "").length < 14) {
+      cnpjInput.focus();
+      return;
+    }
+    // Se possuir um id, edita um dado, senão apenas insere
+    if (values.id) {
+      await CompanyAPI.update(values.id, data);
+    } else {
+      await CompanyAPI.create(data);
+    }
+    reset();
+    closeModal();
+    updateCompanies();
+  }
+
+  async function handleAssignUser(e) {
+    let companyID = values.id;
+    let userID = e.target.value;
+    await CompanyAPI.addUser(companyID, userID);
+    updateCompanies();
+    setTimeout(async () => {
+      await UserAPI.addCompany(userID, companyID);
+      updateUsers();
+    }, 1000);
+  }
+
+  async function handleUnassignUser(id) {
+    let companyID = values.id;
+    let userID = id;
+    await CompanyAPI.removeUser(companyID, userID);
+    updateCompanies();
+    setTimeout(async () => {
+      await UserAPI.removeCompany(userID, companyID);
+      updateUsers();
+    }, 1000);
+  }
+
+  return (
+    <>
+      <Backdrop onClick={closeModal} />
+      <ModalContainer>
+        <Title>
+          {values.name ? `Editar ${values.name}` : "Adicionar Empresa"}
+        </Title>
+
+        <form
+          id="company"
+          onSubmit={handleSubmit((data) => handleSubmitForm(data))}
+        >
+          <label htmlFor="name">Nome*</label>
+          <input
+            type="text"
+            id="name"
+            {...register("name", { required: true })}
+            defaultValue={values.name}
+          />
+          <label htmlFor="cnpj">CNPJ*</label>
+          <InputMask
+            mask="99.999.999/9999-99"
+            type="tel"
+            id="cnpj"
+            {...register("cnpj", { required: true, minLength: 18 })}
+            defaultValue={values.cnpj}
+          />
+          <label htmlFor="address">Endereço*</label>
+          <input
+            type="text"
+            id="address"
+            {...register("address", { required: true })}
+            defaultValue={values.address}
+          />
+          <label htmlFor="users">Usuários</label>
+          <section className={assignedUsers ? "items" : ""}>
+            {assignedUsers.map((user) => {
+              return (
+                <Item key={user.id} onClick={() => handleUnassignUser(user.id)}>
+                  {user.name}
+                  <RemoveIcon />
+                </Item>
+              );
+            })}
+          </section>
+          <select
+            id="users"
+            onChange={handleAssignUser}
+            disabled={values.id && unassignedUsers ? false : true}
+          >
+            {unassignedUsers.length > 0 && <option value={""} />}
+            {unassignedUsers.map((user) => {
+              return (
+                <option key={user.id} value={user.id}>
+                  {user.name}
+                </option>
+              );
+            })}
+          </select>
+          {!unassignedUsers.length > 0 && <sub>não há mais usuários</sub>}
+          {!values.id && <sub>você pode adicionar usuários depois</sub>}
+          <section className="buttons">
+            <Button onClick={closeModal} className="cancel">
+              Cancelar
+            </Button>
+            <input type="submit" className="save" value="Salvar" />
+          </section>
+        </form>
+      </ModalContainer>
+    </>
+  );
+}
---
+++ src/components/UserModal/index.jsx
@@ -0,0 +1,177 @@
+/* eslint-disable no-nonoctal-decimal-escape */
+/* eslint-disable react/prop-types */
+import {
+  Backdrop,
+  ModalContainer,
+  Title,
+  Button,
+  Item,
+} from "../../styles/components/Modal";
+import { RemoveIcon } from "../Icons/RemoveIcon";
+import { useForm } from "react-hook-form";
+import InputMask from "react-input-mask";
+import { useEffect, useState } from "react";
+import UserAPI from "../../services/users";
+import CompanyAPI from "../../services/companies";
+
+export function UserModal({
+  updateUsers,
+  updateCompanies,
+  closeModal,
+  values,
+  users,
+  companies,
+}) {
+  const { register, handleSubmit, reset } = useForm();
+
+  // Filtra empresas vinculadas com o usuário
+  const assignedCompanies =
+    values.id && users
+      ? companies.filter((company) =>
+          users
+            .find((user) => user.id == values.id)
+            .companies.includes(company.id)
+        )
+      : []; // Caso esteja criando um usuário, ele não possui empresas ainda
+
+  //  Filtra empresas que *não estão* vinculadas com o usuário
+  const unassignedCompanies = values.id
+    ? companies.filter(
+        (company) =>
+          !users
+            .find((user) => user.id == values.id)
+            .companies.includes(company.id)
+      )
+    : companies; // Caso esteja criando um usuário, mostra todas empresas
+
+  // Define se é uma inserção ou edição de dado
+  async function handleSubmitForm(data) {
+    // Valida telefone (permite apenas vazio ou completo)
+    let phoneInput = document.querySelector("input#phone");
+    let phoneLength = data.phone.replace(/\D/g, "").length;
+    if (phoneLength > 0 && phoneLength < 11) {
+      phoneInput.focus();
+      return;
+    }
+    // Se possuir um id, edita um dado, senão apenas insere
+    if (values.id) {
+      await UserAPI.update(values.id, data);
+    } else {
+      await UserAPI.create(data);
+    }
+    reset();
+    closeModal();
+    updateUsers();
+  }
+
+  async function handleAssignCompany(e) {
+    let userID = values.id;
+    let companyID = e.target.value;
+    await UserAPI.addCompany(userID, companyID);
+    updateUsers();
+    setTimeout(async () => {
+      await CompanyAPI.addUser(companyID, userID);
+      updateCompanies();
+    }, 1000);
+  }
+
+  async function handleUnassignCompany(id) {
+    let userID = values.id;
+    let companyID = id;
+    await UserAPI.removeCompany(userID, companyID);
+    updateUsers();
+    setTimeout(async () => {
+      await CompanyAPI.removeUser(companyID, userID);
+      updateCompanies();
+    }, 1000);
+  }
+
+  return (
+    <>
+      <Backdrop onClick={closeModal} />
+      <ModalContainer>
+        <Title>
+          {values.name ? `Editar ${values.name}` : "Adicionar Usuário"}
+        </Title>
+
+        <form
+          id="user"
+          onSubmit={handleSubmit((data) => handleSubmitForm(data))}
+        >
+          <label htmlFor="name">Nome*</label>
+          <input
+            type="text"
+            id="name"
+            {...register("name", { required: true })}
+            defaultValue={values.name}
+          />
+          <label htmlFor="mail">E-mail*</label>
+          <input
+            type="email"
+            id="mail"
+            {...register("mail", { required: true })}
+            defaultValue={values.mail}
+          />
+          <label htmlFor="phone">Telefone</label>
+          <InputMask
+            mask="(99)99999-9999"
+            type="tel"
+            id="phone"
+            {...register("phone")}
+            defaultValue={values.phone}
+          />
+          <label htmlFor="birthdate">Data de Nascimento</label>
+          <input
+            type="date"
+            id="birthdate"
+            {...register("birthdate")}
+            defaultValue={values.birthdate}
+          />
+          <label htmlFor="city">Cidade</label>
+          <input
+            type="text"
+            id="city"
+            {...register("city")}
+            defaultValue={values.city}
+          />
+          <label htmlFor="companies">Empresas</label>
+          <section className={assignedCompanies ? "items" : ""}>
+            {assignedCompanies.map((company) => {
+              return (
+                <Item
+                  key={company.id}
+                  onClick={() => handleUnassignCompany(company.id)}
+                >
+                  {company.name}
+                  <RemoveIcon />
+                </Item>
+              );
+            })}
+          </section>
+          <select
+            id="users"
+            onChange={handleAssignCompany}
+            disabled={values.id && unassignedCompanies ? false : true}
+          >
+            {unassignedCompanies.length > 0 && <option value={""} />}
+            {unassignedCompanies.map((company) => {
+              return (
+                <option key={company.id} value={company.id}>
+                  {company.name}
+                </option>
+              );
+            })}
+          </select>
+          {!unassignedCompanies && <sub>não há mais empresas</sub>}
+          {!values.id && <sub>*você pode adicionar empresas depois</sub>}
+          <section className="buttons">
+            <Button onClick={closeModal} className="cancel">
+              Cancelar
+            </Button>
+            <input type="submit" className="save" value="Salvar" />
+          </section>
+        </form>
+      </ModalContainer>
+    </>
+  );
+}
