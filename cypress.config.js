cd8e39b939b4cb6f00938597d66a09cb99fc40ad
MatheusGesser <matheusmvg@hotmail.com>
2023-09-01 10:38:55 -0300

    chore: add configs and dependencies

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportHeight: 1000,
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
